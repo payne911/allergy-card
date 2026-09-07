d224e9971453ba109d31c26403334636e827b3c3
e2e <e2e@test.local>
2026-09-07 23:38:09 +0000

    Clean up duplicate corn/maize slash names in packs; sw v17

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "allergy-card-v16";
+const CACHE = "allergy-card-v17";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./qr.js", "./languages.json.js", "./lang/en.json.js"];
 
 self.addEventListener("install", e => {
